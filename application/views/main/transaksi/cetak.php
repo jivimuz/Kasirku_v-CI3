68eef89359ff1d22ef75c7382b4fdce86e6dbfae
jivimuz <jivirasgal@gmail.com>
2023-05-04 13:52:37 +0700

    fix

--- application/views/main/transaksi/cetak.php
+++ application/views/main/transaksi/cetak.php
@@ -21,28 +21,37 @@
 		sampai 
 		<?=date('d-m-Y', strtotime($date_to)) ?> :</h5>
    	<table class="table " width="100%"  style="font-size:12px">
-   		<tr>
+   		<thead>
+		<tr  bgcolor="#f0f0f0">
    			<th>No.</th>
+   			<th>Waktu Transaksi</th>
    			<th>Nama Kasir</th>
    			<th>Total Harga</th>
-   			<th>Waktu Transaksi</th>
    		</tr>
-
+		</thead>
+		<tbody>
    		<?php $no = 0;
+		$total = 0;
    		if ($data->num_rows() > 0) {
    		 foreach($data->result() as $i) { 
+			$total =$total + $i->total_harga;
    		 	$no++;?>
    		<tr>
    			<td><?= $no;?></td>
+   			<td><?= $i->paid_at;?></td>
    			<td><?= $i->nama_pegawai;?></td>
    			<td>Rp. <?= buatRupiah($i->total_harga);?></td>
-   			<td><?= $i->paid_at;?></td>
    		</tr>
    	<?php }}else{?>
 		<tr>
 			<td colspan="4" class="text-center">Tidak Ada Data</td>
 		</tr>
    	<?php }?>
+	   <tr bgcolor="#f0f0f0">
+			<td colspan="3" class="text-right">Total :</td>
+			<td colspan="1" >Ro. <?=buatRupiah($total)?></td>
+		</tr>
+		</tbody>
    	</table>
    </div>
 </div>
